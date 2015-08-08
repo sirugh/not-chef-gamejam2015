3fd45b6445e5d102b6595aef1e8a9af1813a95f7
Scott McClellan <scott.mcclellan@puppetlabs.com>
2015-08-08 14:19:33 -0500

    Remove phaser.png

--- app/scene/preloader_scene.js
+++ app/scene/preloader_scene.js
@@ -5,9 +5,6 @@ var PreloaderScene = {
     this.loadingBar.anchor.setTo(0, 0.5);
     this.load.setPreloadSprite(this.loadingBar);
 
-    // TODO: load here the assets for the game
-    this.game.load.image('logo', 'images/phaser.png');
-
     // SHITTY PROTOTYPE ASSETS AWAYYYYYYYY
     this.game.load.image('splash', 'images/splash.png')
 
