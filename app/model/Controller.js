a5aabadec42d4d94e16c32670f9409034efdf2cc
Stephen Rugh <stephenrugh@gmail.com>
2015-08-08 17:51:26 -0500

    fix merge conflict

--- app/model/Controller.js
+++ app/model/Controller.js
@@ -10,17 +10,6 @@ var Controller = function (game, player, device) {
 
 Controller.prototype.addButtons = function () {
   var game = this.game;
-<<<<<<< Updated upstream
-=======
-  if (this.device.index === 0) {
-    console.log('creating device 1')
-    this.imageA = game.add.image(200, 300, 'xbox360', '360_A');
-  } else {
-    console.log('creating device 2')
-    this.imageA = game.add.image(500, 300, 'xbox360', '360_A');
-  }
->>>>>>> Stashed changes
-
   //  We can't do this until we know that the gamepad has been connected and is started
   this.buttonA = this.device.getButton(Phaser.Gamepad.XBOX360_A);
   this.buttonA.onDown.add(onDown, this);
